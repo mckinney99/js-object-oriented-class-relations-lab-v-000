1fb1762c4ebbfaaa1d29b2734e153de2444b1be5
Eric McKinney <mckinney99@gmail.com>
2018-02-28 03:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ let store = {drivers:[], passengers:[], trips:[]}
 
 let driverId = 0
 class Driver {
-  constructor(name){
+  constructor (name) {
     this.id = ++driverId
     this.name = name
     store.drivers.push(this)
   }
 
-  trips(){
-    return store.trips.filter(function(trip)){
+  trips () {
+    return store.trips.filter(function(trip){
       return trip.driverId === this.id
     }.bind(this))
   }
-  passengers(){
+  passengers () {
     return store.passengers.filter(function(passenger){
       const driverTrips = this.trips()
       return driverTrips.filter(function(trip){
