c781ee9256b5e2d9c38eb21b11c5536d691c5657
Eric McKinney <mckinney99@gmail.com>
2018-02-28 03:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
+
 let driverId = 0
 class Driver {
   constructor(name){
     this.Id = ++driverId
     this.name = name
     store.drivers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(function(trip)){
+      return trip.driverId === this.id
+    }
   }
 }
