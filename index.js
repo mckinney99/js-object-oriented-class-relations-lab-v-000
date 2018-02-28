0dbe08b06b7e582ea2a147e498ff8f222c10b033
Eric McKinney <mckinney99@gmail.com>
2018-02-28 03:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,49 @@ class Driver {
     }.bind(this))
   }
 }
+
+let passengerId = 0
+class Passenger {
+  constructor (name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+
+  trips(){
+  return store.trips.filter(function(trip){
+    return trip.passengerId === this.id
+  }.bind(this))
+ }
+
+ drivers () {
+   return store.drivers.filter(function(driver){
+     const passengerTrips = this.trips()
+     return passengerTrips.filter(function(trip){
+       return trip.driverId = driver.id
+     })
+   }.bind(this))
+ }
+}
+
+let tripId = 0
+class Trip {
+  constructor (driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+
+  passenger () {
+    return store.passengers.find(function(passenger) {
+    return passenger.id === this.passengerId
+    }.bind(this))
+  }
+
+  driver () {
+    return store.drivers.find(function(driver) {
+    return driver.id === this.driverId
+    }.bind(this))
+  }
+}
